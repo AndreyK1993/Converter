4445924e7da588cd05d762e9f3dfd13bbcdb8732
AndreyK1993 <andrey.kurilenko27@gmail.com>
2023-11-05 10:23:07 +0200

    Add converter mil to km

--- src/app/Converter.java
+++ src/app/Converter.java
@@ -1,4 +1,17 @@
 package app;
 
 public class Converter {
+    public static void main(String[] args) {
+        
+        double miles = 50;
+
+        printMilesToKm(miles);
+    }
+    public static double milesToKm(double miles) {
+        return miles * 1.60934;
+    }
+    public static void printMilesToKm(double miles) {
+        double result = milesToKm(miles);
+        System.out.println(miles + " miles is equal to " + result + " kilometers");
+    }
 }
